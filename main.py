f57891518516a9dd8ebd1a86a3cf48a96fe28643
CrazyJ18 <jsteinke310@gmail.com>
2024-06-20 20:30:24 -0500

    count words

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
+    text = read_file()
+    print(f"{count_words(text)} words in document")
+
+def read_file():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 main()
