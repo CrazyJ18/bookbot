b72537edf3a8d0a458e3a9bdb21a75481c156f85
CrazyJ18 <jsteinke310@gmail.com>
2024-06-21 09:52:58 -0500

    count characters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     text = read_file()
-    print(f"{count_words(text)} words in document")
+    print(count_char(text))
 
 def read_file():
     with open("books/frankenstein.txt") as f:
@@ -11,4 +11,13 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_char(text):
+    char_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
 main()
